5f222e3cdd7c7a7f0955bfcf6d1d9bf1831187ed
Joan Manyà <56130241+manyajoan@users.noreply.github.com>
2020-09-04 12:19:16 +0200

    123 124 125 Ejercicio 7
    Ejercicio 7 Finalizado

--- POO/src/Ejercicio7/Cliente.java
+++ POO/src/Ejercicio7/Cliente.java
@@ -10,59 +10,47 @@ package Ejercicio7;
  * @author conan
  */
 public class Cliente {
-    //Atributos 
+	//Atributos 
 
-    String nombre;
-    String apellido;
-    String dni;
-    Cuenta cuentas[];
+	String nombre;
+	String apellido;
+	String dni;
+	Cuenta cuentas[];
 
-    //Metodos 
-    public Cliente(String nombre, String apellido, String dni, Cuenta[] cuentas) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.cuentas = cuentas;
-    }
+	//Metodos 
+	public Cliente(String nombre, String apellido, String dni, Cuenta[] cuentas) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.cuentas = cuentas;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public String getApellido() {
+		return apellido;
+	}
 
-    public String getDni() {
-        return dni;
-    }
+	public String getDni() {
+		return dni;
+	}
 
-    public double consultarSaldo(int n) {
-        double saldo = 0;
-        for (int i = 0; i < cuentas.length; i++) {
-            if (cuentas[i].numeroCuenta == n) {
-                System.out.println("El numero de cuenta es: " + n);
-                saldo = cuentas[i].getSaldo();
-            }
-        }
-        return saldo;
-    }
+	public double consultarSaldo(int n) {
 
-    public void ingresarDinero(int n, double cantidad) {
-        for (int i = 0; i < cuentas.length; i++) {
-            if (cuentas[i].numeroCuenta == n) {
-                cuentas[i].ingresarDinero(cantidad);
-            }
-        }
+		return cuentas[n].getSaldo();
 
-    }
+	}
 
-    public void retirarDinero(int n, double cantidad) {
-        for (int i = 0; i < cuentas.length; i++) {
-           if (cuentas[i].numeroCuenta == n) {
-               cuentas[i].retirarDinero(cantidad);
-           }
-        }
-    }
+	public void ingresarDinero(int n, double cantidad) {
 
+		cuentas[n].ingresarDinero(cantidad);
+	}
+
+	public void retirarDinero(int n, double cantidad) {
+
+		cuentas[n].retirarDinero(cantidad);
+
+	}
 }
